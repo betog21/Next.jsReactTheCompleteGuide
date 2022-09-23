b0214a998db01fd6448c7c9583306ee644a1e962
Alberto González <alberto.gonzalez@distillery.com>
2022-09-23 10:19:13 -0500

    The all events page was optimized.

--- Events/pages/events/index.js
+++ Events/pages/events/index.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-function EventsPage() {
-  const events = getAllEvents();
+function EventsPage(props) {
+  const { events } = props;
   const router = useRouter();
+
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
@@ -18,6 +19,16 @@ function EventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default EventsPage;
